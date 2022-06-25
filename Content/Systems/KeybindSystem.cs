df0c791275afb803e99ca8c1f3e384e280d85d05
Jean <jean970039@gmail.com>
2022-06-25 00:45:23 -0700

    Removed outdated comments

--- Content/Systems/KeybindSystem.cs
+++ Content/Systems/KeybindSystem.cs
@@ -4,7 +4,6 @@ using Terraria.ModLoader;
 namespace Octopus.Content.Systems
 {
 		// Acts as a container for keybinds registered by this mod.
-		// See Common/Players/ExampleKeybindPlayer for usage.
 		public class KeybindSystem : ModSystem
 		{
 				public static ModKeybind WaterSpitKeybind { get; private set; }
@@ -19,11 +18,8 @@ namespace Octopus.Content.Systems
 						InkBombKeybind = KeybindLoader.RegisterKeybind(Mod, "Fire Ink Bomb", Keys.X);
 						OrangeFortKeybind = KeybindLoader.RegisterKeybind(Mod, "Summon Orange Fort", Keys.C);
 				}
-
-				// Please see ExampleMod.cs' Unload() method for a detailed explanation of the unloading process.
 				public override void Unload()
 				{
-						// Not required if your AssemblyLoadContext is unloading properly, but nulling out static fields can help you figure out what's keeping it loaded.
 						WaterSpitKeybind = null;
 						InkBombKeybind = null;
 						OrangeFortKeybind = null;
